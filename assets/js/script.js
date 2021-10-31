54517ca7ab9c6b3b93efad09f43e3cacfdcd66ad
ibnusyawall <website.isas.24@gmail.com>
2021-10-31 13:54:40 +0700

    fix: typo in bahasa_planet logic function

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,15 +55,17 @@ $(document).ready(function() {
 
 function bahasa_planet(text, alias) {
     let result = ''
-    result += (
-        (
-            (/A|a/.test(v)) ? v.replace(/A|a/g, `${v}${alias}${v}`) :
-            (/I|i/.test(v)) ? v.replace(/I|i/g, `${v}${alias}${v}`) :
-            (/U|u/.test(v)) ? v.replace(/U|u/g, `${v}${alias}${v}`) :
-            (/E|e/.test(v)) ? v.replace(/E|e/g, `${v}${alias}${v}`) :
-            (/O|o/.test(v)) ? v.replace(/O|o/g, `${v}${alias}${v}`) :
-            v
-        ))
+    text.split``.map(v => {
+        result += (
+            (
+                (/A|a/.test(v)) ? v.replace(/A|a/g, `${v}${alias}${v}`) :
+                (/I|i/.test(v)) ? v.replace(/I|i/g, `${v}${alias}${v}`) :
+                (/U|u/.test(v)) ? v.replace(/U|u/g, `${v}${alias}${v}`) :
+                (/E|e/.test(v)) ? v.replace(/E|e/g, `${v}${alias}${v}`) :
+                (/O|o/.test(v)) ? v.replace(/O|o/g, `${v}${alias}${v}`) :
+                v
+            )
+        )
     })
     return result
 }
